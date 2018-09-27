d263dcb40eb30e7d1c347e9ed05d3a6689c284ea
Robert Schweda <schweda@itemis.de>
2018-09-27 14:28:11 +0200

    Added simple deploy script for local deployment

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+,/gradlew clean installDist
+heroku container:push web -a si-devcon-alexa-setup
+heroku container:release web -a si-devcon-alexa-setup
--- src/main/java/de/signaliduna/alexa/AlexaSkill.java
+++ src/main/java/de/signaliduna/alexa/AlexaSkill.java
@@ -2,16 +2,29 @@ package de.signaliduna.alexa;
 
 import de.signaliduna.alexa.rest.HelloWorld;
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class AlexaSkill extends Application<AlexaSkillConfiguration> {
 
 	public static void main(String args[]) throws Exception {
+		System.out.println("PORT:" +  System.getenv("PORT"));
+		System.setProperty("dw.server.connector.port", System.getenv("PORT"));
+
 		new AlexaSkill().run(args);
+	}
+
+	public void initialize(Bootstrap<AlexaSkillConfiguration> bootstrap) {
+		bootstrap.setConfigurationSourceProvider(
+				new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+						new EnvironmentVariableSubstitutor()
+				)
+		);
 	}
 
 	@Override public void run(AlexaSkillConfiguration configuration, Environment environment) throws Exception {
 		environment.jersey().register(HelloWorld.class);
 	}
-
 }
