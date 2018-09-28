91ab178674ce888732cd3e07732952a52bcb594d
Robert Schweda <schweda@itemis.de>
2018-09-28 11:20:35 +0200

    Added logic to postgres call

--- src/main/java/de/signaliduna/alexa/rest/HelloWorld.java
+++ src/main/java/de/signaliduna/alexa/rest/HelloWorld.java
@@ -3,10 +3,9 @@ package de.signaliduna.alexa.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 @Path("hello")
 public class HelloWorld {
@@ -19,15 +18,22 @@ public class HelloWorld {
 
 	@Path("database")
 	@GET
-	public String getDatabaseConnection() {
-		String dbUrl = System.getenv("JDBC_DATABASE_URL");
-		try {
-			Connection connection = DriverManager.getConnection(dbUrl);
-			return connection.getSchema();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return "Connection failed";
+	public String getDatabaseConnection() throws SQLException, URISyntaxException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+		Connection connection = DriverManager.getConnection(dbUrl, username, password);
+		DatabaseMetaData md = connection.getMetaData();
+		ResultSet rs = md.getTables(null, null, "%", null);
+		StringBuilder resultBuilder = new StringBuilder();
+
+		while (rs.next()) {
+			resultBuilder.append(rs.getString(3));
+			resultBuilder.append("\n");
 		}
+
+		return resultBuilder.toString();
 	}
 
 	@Path("voice")
