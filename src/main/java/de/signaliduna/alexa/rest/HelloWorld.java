d75b6438aef734193491d7b0e9e79fd0314f4076
Robert Schweda <schweda@itemis.de>
2018-09-28 11:07:39 +0200

    Added postgres call

--- src/main/java/de/signaliduna/alexa/rest/HelloWorld.java
+++ src/main/java/de/signaliduna/alexa/rest/HelloWorld.java
@@ -3,6 +3,10 @@ package de.signaliduna.alexa.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Path("hello")
 public class HelloWorld {
@@ -13,6 +17,18 @@ public class HelloWorld {
 		return "Hello nerds!";
 	}
 
+	@Path("database")
+	@GET
+	public String getDatabaseConnection() {
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		try {
+			Connection connection = DriverManager.getConnection(dbUrl);
+			return connection.getSchema();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "Connection failed";
+		}
+	}
 
 	@Path("voice")
 	@POST
