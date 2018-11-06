05f41c1cda618536c83939fe9ffe48e2be44343d
sidevcon <sidevconsoltau@gmail.com>
2018-11-06 21:37:41 +0100

    ...

--- src/main/java/de/signaliduna/alexa/handlers/HelpIntentHandler.java
+++ src/main/java/de/signaliduna/alexa/handlers/HelpIntentHandler.java
@@ -25,7 +25,7 @@ import static com.amazon.ask.request.Predicates.intentName;
 public class HelpIntentHandler implements RequestHandler {
     @Override
     public boolean canHandle(HandlerInput input) {
-        return input.matches(intentName("AMAZON.HelpIntent")));
+        return input.matches(intentName("AMAZON.HelpIntent"));
     }
 
     @Override
