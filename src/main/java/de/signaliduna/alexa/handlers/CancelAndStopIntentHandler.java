89eca4e849d379de860afcb1bcffa65dfd050311
Mucahid <mucahid.yekta@signal-iduna.de>
2018-11-06 17:51:17 +0100

    Added cat sound

--- src/main/java/de/signaliduna/alexa/handlers/CancelAndStopIntentHandler.java
+++ src/main/java/de/signaliduna/alexa/handlers/CancelAndStopIntentHandler.java
@@ -21,7 +21,7 @@ public class CancelAndStopIntentHandler implements RequestHandler {
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
         return handlerInput.getResponseBuilder()
-                .withSpeech("<amazon:effect name=\"whispered\">See you later, alligator.</amazon:effect>")
+                .withSpeech("See you later, <amazon:effect name=\"whispered\">alligator.</amazon:effect><audio src='soundbank://soundlibrary/animals/amzn_sfx_cat_long_meow_1x_01'/>")
                 .withSimpleCard("HelloWorld", "Ende aus Micky Maus.")
                 .build();
     }
