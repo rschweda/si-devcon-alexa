d346e36f7b9fa5b54d9d4d8c66cb7421663a2005
Aylin Erben <aylin.erben@signal-iduna.de>
2018-11-07 12:53:34 +0100

    added image

--- src/main/java/de/signaliduna/alexa/handlers/HelpIntentHandler.java
+++ src/main/java/de/signaliduna/alexa/handlers/HelpIntentHandler.java
@@ -22,7 +22,7 @@ public class HelpIntentHandler implements RequestHandler {
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
 
-        Image img =  Image.builder().withSmallImageUrl("https://img.fotocommunity.com/kaputte-fenster-die-3-0c5a6da5-9105-4040-9d0d-1a9f35d4eb57.jpg?height=1080").build();
+        Image img =  Image.builder().withLargemageUrl("https://img.fotocommunity.com/kaputte-fenster-die-3-0c5a6da5-9105-4040-9d0d-1a9f35d4eb57.jpg?height=1080").build();
 
         return handlerInput.getResponseBuilder()
                 .withSpeech("Um einen Schaden zu melden sagen sie einfach: Schaden melden. Ich werde dann alle notwendigen Informationen abfragen und Ihre Meldung an einen Mitarbeiter der Signal Iduna weiterleiten. <amazon:effect name=\"whispered\">vielleicht</amazon:effect>")
