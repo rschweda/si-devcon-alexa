89f267d8ab854c4f3b750bdb2af07fc7d77fa45b
Mucahid <mucahid.yekta@signal-iduna.de>
2018-11-07 09:56:36 +0100

    added slots as result string

--- src/main/java/de/signaliduna/alexa/handlers/SchadenmeldungsIntentHandler.java
+++ src/main/java/de/signaliduna/alexa/handlers/SchadenmeldungsIntentHandler.java
@@ -2,15 +2,13 @@ package de.signaliduna.alexa.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.DialogState;
-import com.amazon.ask.model.Intent;
-import com.amazon.ask.model.IntentRequest;
-import com.amazon.ask.model.Response;
+import com.amazon.ask.model.*;
 import com.amazon.ask.model.dialog.DelegateDirective;
 import de.signaliduna.alexa.AlexaSkillConfiguration;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.Map;
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
@@ -43,9 +41,15 @@ public class SchadenmeldungsIntentHandler implements RequestHandler {
 			return input.getResponseBuilder().addDirective(DelegateDirective.builder().withUpdatedIntent(myIntentRequest.getIntent()).build()).build();
 		} else {
 			System.out.println(((IntentRequest) input.getRequestEnvelope().getRequest()).getDialogState());
+            Map<String, Slot> slots = ((IntentRequest) input.getRequestEnvelope().getRequest()).getIntent().getSlots();
+            System.out.println("MYSLOTS :" + slots.toString());
+            String result = "";
+            for( String key : slots.keySet()){
+                result += key + " " + slots.get(key) + " - ";
+            }
 			return input.getResponseBuilder()
 					.withSpeech("Vielen Dank. Wir melden uns in Kürze bei Ihnen.")
-					.withSimpleCard("Schadensmeldung","Vielen Dank. Wir melden uns in Kürze bei Ihnen. <amazon:effect name=\"whispered\">Gut zu wissen, dass es SIGNAL IDUNA gibt</amazon:effect>" )
+					.withSimpleCard("Schadensmeldung","Vielen Dank. Wir melden uns in Kürze bei Ihnen. Zusammenfassung ["+  result+"]. <amazon:effect name=\"whispered\">Gut zu wissen, dass es SIGNAL IDUNA gibt</amazon:effect>" )
 					.build();
 		}
 
